25178b150d01c0b5b2504c8a1e43015b7c12e136
mulewile <william.chanda@posteo.de>
2024-03-15 17:56:25 +0100

    added config.json file where environmental variables are stored. - refactored connect.php to get database credentials from config.json

--- src/connect.php
+++ src/connect.php
@@ -1,10 +1,43 @@
 <?php
-require_once '../config/config.php';
+//The vairale stores the path to the json configuration file
+$config_json_path   = '../config/config.json';
+$json_config_data   = decode_config_json($config_json_path);
 
-try {
-    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-    echo "You have successfully connected to the database";
-} catch (PDOException $pe) {
-    die("Could not connect to the database $dbname :" . $pe->getMessage());
+//Get the environmental variables from the first object in the json configuration file
+$environmental_variables_object = $json_config_data[0];
+
+//Get environmental variables from the environmental_variables_object
+$host     = $environmental_variables_object['host'];
+$database = $environmental_variables_object['database_name'];
+$username = $environmental_variables_object['username'];
+$password = $environmental_variables_object['password'];
+
+
+//Establish a connection to the database
+//PDO stands for PHP Data Objects.
+$database_handle = new PDO("mysql:host=$host;dbname=$database", $username, $password);
+
+
+//Check if the connection was successful and give the user feedback
+if(!$database_handle){
+    echo "Connection failed. Contact your system administrator";
+    die("Connection failed: " . mysqli_connect_error());
+}else{
+    echo "Connected successfully";
 }
 
+//This function decodes the json configuration file which contains the environmental variables for the database connection
+//Parameters: $config_json_path - the path to the json configuration file
+function decode_config_json($config_json_path){
+
+    if(!file_exists($config_json_path)){
+        echo "The json configuration file does not exist or the path is incorrect. Please contact your system administrator.";
+        return;
+    }else{
+        $json_config_data = file_get_contents($config_json_path);
+        $json_config_data = json_decode($json_config_data, true);
+        return $json_config_data;
+    }
+}
+
+?>
